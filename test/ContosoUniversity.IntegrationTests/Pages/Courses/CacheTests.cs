606144cd2976690f3a851a3070c4bcda261c67d8
Adam Anderly <adam@anderly.com>
2025-03-24 12:22:07 -0500

    Update cache test to test cached course info.

--- test/ContosoUniversity.IntegrationTests/Pages/Courses/CacheTests.cs
+++ test/ContosoUniversity.IntegrationTests/Pages/Courses/CacheTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using ContosoUniversity.Models;
 using Shouldly;
@@ -17,9 +16,9 @@ public class CacheTests
     public CacheTests(SliceFixture fixture) => _fixture = fixture;
 
     [Fact]
-    public async Task Should_return_all_courses()
+    public async Task Should_return_cached_course_title_when_update_does_not_go_through_command()
     {
-        var adminId = await _fixture.SendAsync(
+		var adminId = await _fixture.SendAsync(
             new ContosoUniversity.Pages.Instructors.CreateEdit.Command
             {
                 FirstMidName = "George",
@@ -34,14 +33,6 @@ public class CacheTests
             Budget = 123m,
             StartDate = DateTime.Today
         };
-        var historyDept = new Department
-        {
-            Name = "History",
-            InstructorId = adminId,
-            Budget = 123m,
-            StartDate = DateTime.Today
-        };
-
         var english = new Course
         {
             Credits = 4,
@@ -49,26 +40,120 @@ public class CacheTests
             Id = _fixture.NextCourseNumber(),
             Title = "English 101"
         };
-        var history = new Course
-        {
-            Credits = 4,
-            Department = historyDept,
-            Id = _fixture.NextCourseNumber(),
-            Title = "History 101"
-        };
         await _fixture.InsertAsync(
-            englishDept, 
-            historyDept, 
-            english, 
-            history);
+	        englishDept,
+	        english);
+        
+        var cachedResult = await _fixture.SendAsync(new Details.Query { Id = english.Id });
 
-        var result = await _fixture.SendAsync(new Index.Query());
+		cachedResult.ShouldNotBeNull();
 
-        result.ShouldNotBeNull();
-        result.Courses.Count.ShouldBeGreaterThanOrEqualTo(2);
+        var cachedTitle = english.Title;
+        cachedTitle.ShouldBe(english.Title);
 
-        var courseIds = result.Courses.Select(c => c.Id).ToList();
-        courseIds.ShouldContain(english.Id);
-        courseIds.ShouldContain(history.Id);
+		await _fixture.ExecuteDbContextAsync(async (ctxt, mediator) =>
+		{
+			var course = await _fixture.FindAsync<Course>(english.Id);
+
+			course.Title = "English 202";
+			ctxt.Courses.Update(course);
+
+		});
+
+		var result = await _fixture.SendAsync(new Details.Query { Id = english.Id });
+
+		result.Title.ShouldBe(cachedTitle);
+
+		Edit.Command command = default;
+
+		await _fixture.ExecuteDbContextAsync(async (ctxt, mediator) =>
+		{
+			command = new Edit.Command
+			{
+				Id = english.Id,
+				Credits = english.Credits,
+				Title = "English 202",
+			};
+
+			await mediator.Send(command);
+
+		});
+
+		var edited = await _fixture.SendAsync(new Details.Query { Id = english.Id });
+
+		edited.ShouldNotBeNull();
+		edited.Credits.ShouldBe(command.Credits.GetValueOrDefault());
+		edited.Title.ShouldBe(command.Title);
+	}
+
+    [Fact]
+    public async Task Should_return_updated_course_title_when_update_goes_through_command()
+    {
+	    var adminId = await _fixture.SendAsync(
+		    new ContosoUniversity.Pages.Instructors.CreateEdit.Command
+		    {
+			    FirstMidName = "George",
+			    LastName = "Jones",
+			    HireDate = DateTime.Today
+		    });
+
+	    var englishDept = new Department
+	    {
+		    Name = "English",
+		    InstructorId = adminId,
+		    Budget = 123m,
+		    StartDate = DateTime.Today
+	    };
+	    var english = new Course
+	    {
+		    Credits = 4,
+		    Department = englishDept,
+		    Id = _fixture.NextCourseNumber(),
+		    Title = "English 101"
+	    };
+	    await _fixture.InsertAsync(
+		    englishDept,
+		    english);
+
+	    var cachedResult = await _fixture.SendAsync(new Details.Query { Id = english.Id });
+
+	    cachedResult.ShouldNotBeNull();
+
+	    var cachedTitle = english.Title;
+	    cachedTitle.ShouldBe(english.Title);
+
+	    await _fixture.ExecuteDbContextAsync(async (ctxt, mediator) =>
+	    {
+		    var course = await _fixture.FindAsync<Course>(english.Id);
+
+		    course.Title = "English 202";
+		    ctxt.Courses.Update(course);
+
+	    });
+
+	    var result = await _fixture.SendAsync(new Details.Query { Id = english.Id });
+
+	    result.Title.ShouldBe(cachedTitle);
+
+	    Edit.Command command = default;
+
+	    await _fixture.ExecuteDbContextAsync(async (ctxt, mediator) =>
+	    {
+		    command = new Edit.Command
+		    {
+			    Id = english.Id,
+			    Credits = english.Credits,
+			    Title = "English 202",
+		    };
+
+		    await mediator.Send(command);
+
+	    });
+
+	    var edited = await _fixture.SendAsync(new Details.Query { Id = english.Id });
+
+	    edited.ShouldNotBeNull();
+	    edited.Credits.ShouldBe(command.Credits.GetValueOrDefault());
+	    edited.Title.ShouldBe(command.Title);
     }
 }
